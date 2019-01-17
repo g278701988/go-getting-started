e21d3f8ee01e823c43e208425705d479f0662af5
g278701988 <35322051+g278701988@users.noreply.github.com>
2019-01-17 15:26:37 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+
+var configFileName string
+
+func displayIP(responseWriter http.ResponseWriter, request *http.Request) {
+
+	var jsonData Jsdata
+	ModifyConfig(configFileName, &jsonData, true)
+	//log.Println(jsonData)
+
+	fmt.Fprintf(responseWriter, "ip:%s", jsonData.Value)
+
+}
+func updateIP(responseWriter http.ResponseWriter, request *http.Request) {
+
+	ip, _, err := net.SplitHostPort(request.RemoteAddr)
+	if err != nil {
+
+		fmt.Fprintf(responseWriter, "userip: %q is not IP:port", request.RemoteAddr)
+	}
+	var jsonData = Jsdata{Key: "ip", Value: ip}
+	ModifyConfig(configFileName, &jsonData, false)
+	//log.Println(jsonData)
+	fmt.Fprintf(responseWriter, "%s!", "ok")
+
+}
+func main() {
+
+	addr, err := determineListenAddress()
+  if err == nil {
+  
+  Path, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if nil == err {	
+		log.Println(Path)
+		log.Println("web begin")
+		ConfigName := "config.txt"
+		var jsonData = Jsdata{Key: "ip", Value: ""}
+		configFileName = Path + "/" + ConfigName
+		ModifyConfig(Path+"/"+ConfigName, &jsonData, false)
+		log.Println(jsonData)
+		mux := http.NewServeMux()
+		mux.HandleFunc("/updateip", updateIP)
+		mux.HandleFunc("/", displayIP)
+		server := &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		}
+		err = server.ListenAndServe()
+		if nil != err {
+			log.Fatal(err)
+		}
+
+	}
+	else{
+		log.Fatal(err)
+	}
+
+   
+  }
+  
+	
+	log.Println("web end")
+}
