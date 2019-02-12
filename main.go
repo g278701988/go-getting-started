1de01f3ecc378797b7a5d3c2dff032fc0943591b
g278701988 <35322051+g278701988@users.noreply.github.com>
2019-02-12 13:40:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"	
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"encoding/json"
 )
 
 func determineListenAddress() (string, error) {
@@ -18,6 +17,7 @@ func determineListenAddress() (string, error) {
 }
 
 var configFileName string
+
 func getGithubConfig() string {
 
 	resp, err := http.Get("https://raw.githubusercontent.com/g278701988/go-getting-started/master/config.txt")
@@ -25,36 +25,22 @@ func getGithubConfig() string {
 		log.Printf("Get err%v", err)
 		return string("")
 	}
-	var jsData Jsdata
+	var jsData []Jsdata
 	if err := json.NewDecoder(resp.Body).Decode(&jsData); nil != err {
 		log.Printf("Decode err%v", err)
 		return string("")
 	}
-	return jsData.Value
+
+	return fmt.Sprintf("%v:%v\n%v:%v ", jsData[0].Key, jsData[0].Value, jsData[1].Key, jsData[1].Value)
 }
 
 func displayIP(responseWriter http.ResponseWriter, request *http.Request) {
 
-	var jsonData Jsdata
-	ModifyConfig(configFileName, &jsonData, true)
-	//log.Println(jsonData)
-
 	ip2 := getGithubConfig()
-	fmt.Fprintf(responseWriter, "ip:%s\nip:%s\n", jsonData.Value,ip2)
+	fmt.Fprintf(responseWriter, "%s\t(data will update when ip changed)", ip2)
 
 }
-func updateIP(responseWriter http.ResponseWriter, request *http.Request) {
 
-	ip, ok := request.URL.Query()["ip"]
-	if ok {
-		var jsonData = Jsdata{Key: "ip", Value: ip[0]}
-		ModifyConfig(configFileName, &jsonData, false)
-		//log.Println(jsonData)
-		fmt.Fprintf(responseWriter, "%s!", "ok")
-
-	}
-
-}
 func main() {
 
 	addr, err := determineListenAddress()
@@ -62,27 +48,15 @@ func main() {
 		log.Fatal(err)
 		return
 	}
-	Path, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if nil == err {
-		log.Println(Path)
-		log.Println("web begin")
-		ConfigName := "config.txt"
-		var jsonData = Jsdata{Key: "ip", Value: ""}
-		configFileName = Path + "/" + ConfigName
-		ModifyConfig(Path+"/"+ConfigName, &jsonData, false)
-		log.Println(jsonData)
-		mux := http.NewServeMux()
-		mux.HandleFunc("/updateip", updateIP)
-		mux.HandleFunc("/", displayIP)
-		server := &http.Server{
-			Addr:    addr,
-			Handler: mux,
-		}
-		err = server.ListenAndServe()
-		if nil != err {
-			log.Fatal(err)
-		}
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", displayIP)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+	err = server.ListenAndServe()
+	if nil != err {
+		log.Fatal(err)
 	}
 
 	log.Println("web end")
